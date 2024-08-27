f7fab7e4d870279f22e9e1a67ca61dff67e48147
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-08-27 14:59:59 +0200

    chore: Refactor MySQL initialization script for better error handling and logging

--- container/mysql_init.sh
+++ container/mysql_init.sh
@@ -27,45 +27,38 @@ if [[ -f "/initdb.d/lamp.sql" ]]; then
 else
     echo "=> No lamp.sql found we do the mysql init"
 
+    # if [[ -e /db/init.sql ]]; then
+    #     echo "=> Initializing the database"
+
+    #     mysql -uroot < /db/init.sql
+    # fi
+
     PASS=${MYSQL_ADMIN_PASS:-$(pwgen -s 12 1)}
     _word=$( [ ${MYSQL_ADMIN_PASS} ] && echo "preset" || echo "random" )
     echo "=> Creating MySQL admin user with password"
-
     mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
     mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
     mysql -uroot -e "CREATE DATABASE IF NOT EXISTS phpmyadmin"
-    # mysql -uroot -e "CREATE USER 'pma'@'localhost' IDENTIFIED BY ''"
-    # mysql -uroot -e " GRANT ALL PRIVILEGES ON phpmyadmin.* TO  'pma'@'localhost'"
-    mysql -uroot -e "CREATE USER 'version_user'@'localhost' IDENTIFIED BY 'version_password'";
-    mysql -uroot -e "GRANT SELECT ON mysql.version TO 'version_user'@'localhost'"
 
-    # Create a new database and for phpmyadmin
+    echo "create version_user for mysql version"
+    mysql -uroot -e "CREATE USER 'version_user'@'localhost' IDENTIFIED BY 'version_password'";
+    mysql -uroot -e "GRANT USAGE ON *.* TO 'version_user'@'localhost';
+"
+
+    echo "Create a new database and for phpmyadmin"
     mysql -uroot < /var/www/phpMyAdmin-*/sql/create_tables.sql
 
-    CREATE_MYSQL_USER=false
-
-    if [ -n "$CREATE_MYSQL_BASIC_USER_AND_DB" ] || \
-    [ -n "$MYSQL_USER_NAME" ] || \
-    [ -n "$MYSQL_USER_DB" ] || \
-    [ -n "$MYSQL_USER_PASS" ]; then
-        CREATE_MYSQL_USER=true
-    fi
+    CREATE_MYSQL_USER="False"
 
     if [ "$CREATE_MYSQL_USER" == "True" ]; then
         _user=${MYSQL_USER_NAME:?}
         _userdb=${MYSQL_USER_DB:?}
         _userpass=${MYSQL_USER_PASS:?}
-
+        echo "create a new user  ${_user} with  database ${_userdb}"
         mysql -uroot -e "CREATE USER '${_user}'@'%' IDENTIFIED BY  '${_userpass}'"
         mysql -uroot -e "GRANT USAGE ON *.* TO  '${_user}'@'%' IDENTIFIED BY '${_userpass}'"
         mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${_userdb}"
         mysql -uroot -e "GRANT ALL PRIVILEGES ON ${_userdb}.* TO '${_user}'@'%'"
-    fi
-
-    if [[ -e /db/init.sql ]]; then
-        echo "=> Initializing the database"
-
-        mysql -uroot < /db/init.sql
     fi
 
     echo "=> Done!"
