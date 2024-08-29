9041fc2e2f9ecc68623ada319b0550c76a1d2d21
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-08-29 16:57:16 +0200

    Display  PHP version and improve code formatting in index.php

--- container/index.php
+++ container/index.php
@@ -2,32 +2,33 @@
 $db = new PDO('mysql:host=localhost', 'version_user', 'version_password');
 
 function getOSInformation()
- {
-     if (false == function_exists("shell_exec") || false == is_readable("/etc/os-release")) {
-         return null;
-     }
+{
+    if (false == function_exists("shell_exec") || false == is_readable("/etc/os-release")) {
+        return null;
+    }
 
-      $os         = shell_exec('cat /etc/os-release');
-     $listIds    = preg_match_all('/.*=/', $os, $matchListIds);
-     $listIds    = $matchListIds[0];
+    $os = shell_exec('cat /etc/os-release');
+    $listIds = preg_match_all('/.*=/', $os, $matchListIds);
+    $listIds = $matchListIds[0];
 
-      $listVal    = preg_match_all('/=.*/', $os, $matchListVal);
-     $listVal    = $matchListVal[0];
+    $listVal = preg_match_all('/=.*/', $os, $matchListVal);
+    $listVal = $matchListVal[0];
 
-      array_walk($listIds, function(&$v, $k){
-         $v = strtolower(str_replace('=', '', $v));
-     });
+    array_walk($listIds, function (&$v, $k) {
+        $v = strtolower(str_replace('=', '', $v));
+    });
 
-      array_walk($listVal, function(&$v, $k){
-         $v = preg_replace('/=|"/', '', $v);
-     });
+    array_walk($listVal, function (&$v, $k) {
+        $v = preg_replace('/=|"/', '', $v);
+    });
 
-      return array_combine($listIds, $listVal);
- }
+    return array_combine($listIds, $listVal);
+}
 $osInfo = getOSInformation();
 ?>
 <!doctype html>
 <html lang=en>
+
 <head>
     <meta charset=utf-8>
     <title>Hello World from NS8-LAMP</title>
@@ -35,8 +36,14 @@ $osInfo = getOSInformation();
     <style>
         @import 'https://fonts.googleapis.com/css?family=Montserrat|Raleway|Source+Code+Pro';
 
-        body { font-family: 'Raleway', sans-serif; }
-        h2 { font-family: 'Montserrat', sans-serif; }
+        body {
+            font-family: 'Raleway', sans-serif;
+        }
+
+        h2 {
+            font-family: 'Montserrat', sans-serif;
+        }
+
         pre {
             font-family: 'Source Code Pro', monospace;
 
@@ -57,25 +64,31 @@ $osInfo = getOSInformation();
         }
     </style>
 </head>
+
 <body>
     <div class="container">
         <header>
-            <h2>Welcome to <a href="https://github.com/stephdl/ns8-lamp" target="_blank">ns8-Lamp</a> a.k.a stephdl/ns8-lamp</h2>
+            <h2>Welcome to <a href="https://github.com/stephdl/ns8-lamp" target="_blank">ns8-Lamp</a> a.k.a
+                stephdl/ns8-lamp</h2>
         </header>
         <article>
             <p>
                 For documentation, <a href="https://github.com/stephdl/ns8-lamp" target="_blank">click here</a>.
+            </p>
+            <p>
+                For PHP information (Please remove the phpinfo.phpp file), <a href="phpinfo.php" target="_blank">click here</a>.
             </p>
         </article>
         <section>
             <pre>
 OS: <?php echo $osInfo['pretty_name']; ?><br/>
 Apache: <?php echo apache_get_version(); ?><br/>
-MySQL Version: <?php echo $db->getAttribute( PDO::ATTR_SERVER_VERSION ); ?><br/>
+MySQL Version: <?php echo $db->getAttribute(PDO::ATTR_SERVER_VERSION); ?><br/>
 PHP Version: <?php echo phpversion(); ?><br/>
 phpMyAdmin Version: <?php echo getenv('PHPMYADMIN_VERSION'); ?>
             </pre>
         </section>
     </div>
 </body>
+
 </html>
