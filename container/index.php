a302ec5ed569ecaedd2fe4cd03d23464df70bcca
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-08-29 17:24:01 +0200

    Improve code formatting and add warning message in index.php

--- container/index.php
+++ container/index.php
@@ -1,3 +1,31 @@
+<?php
+$db = new PDO('mysql:host=localhost', 'version_user', 'version_password');
+
+function getOSInformation()
+{
+    if (false == function_exists("shell_exec") || false == is_readable("/etc/os-release")) {
+        return null;
+    }
+
+    $os = shell_exec('cat /etc/os-release');
+    $listIds = preg_match_all('/.*=/', $os, $matchListIds);
+    $listIds = $matchListIds[0];
+
+    $listVal = preg_match_all('/=.*/', $os, $matchListVal);
+    $listVal = $matchListVal[0];
+
+    array_walk($listIds, function (&$v, $k) {
+        $v = strtolower(str_replace('=', '', $v));
+    });
+
+    array_walk($listVal, function (&$v, $k) {
+        $v = preg_replace('/=|"/', '', $v);
+    });
+
+    return array_combine($listIds, $listVal);
+}
+$osInfo = getOSInformation();
+?>
 <!doctype html>
 <html lang=en>
 
@@ -77,5 +105,4 @@ phpMyAdmin Version: <?php echo getenv('PHPMYADMIN_VERSION'); ?>
         </section>
     </div>
 </body>
-
 </html>
